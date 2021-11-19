6ff946ba47a8eb056419435cbe999152dd004aa9
Pang <yuwenpan@ad.helsinki.fi>
2021-11-19 08:56:21 +0200

    Upload the processes human.csv

---
+++ data/create_human.R
@@ -0,0 +1,110 @@
+# Excise 5
+# Data wrangling for human data
+
+# Author; Yuwen Pang
+# Date: 16/11/2021
+
+# 1 Load the library
+library(stringr)
+library(dplyr)
+
+# Set the working directory
+setwd("Z:/Course/Opendata science/IODS-project")
+
+# 2 Read the “Human development” and “Gender inequality” datas into R.
+# The data combines several indicators from most countries in the world
+hd <- read.csv("http://s3.amazonaws.com/assets.datacamp.com/production/course_2218/datasets/human_development.csv", stringsAsFactors = F)
+gii <- read.csv("http://s3.amazonaws.com/assets.datacamp.com/production/course_2218/datasets/gender_inequality.csv", stringsAsFactors = F, na.strings = "..")
+
+# 3 Explore the datasets
+# the structure and dimensions of the data
+str(hd);str(gii)
+dim(hd);dim(gii)
+summary(hd);summary(gii)
+
+# remove the commas from GNI and print out a numeric version of it
+str_replace(human$GNI, pattern=",", replace ="") %>% as.numeric()
+
+# 4 rename the variables
+# "Country" = Country name
+# Health and knowledge
+# "GNI" = Gross National Income per capita
+# 'MYE' = "Mean.Years.of.Education"
+# "Life.Exp" = Life expectancy at birth
+# "Edu.Exp" = Expected years of schooling 
+colnames(hd) <- c('HDI.R','Country','HDI','Life.Exp','Edu.Exp','MYE','GNI','GNI.R')
+
+# Empowerment
+# "Mat.Mor" = Maternal mortality ratio
+# "Ado.Birth" = Adolescent birth rate
+# "Parli.F" = Percetange of female representatives in parliament
+# "Edu2.F" = Proportion of females with at least secondary education
+# "Edu2.M" = Proportion of males with at least secondary education
+# "Labo.F" = Proportion of females in the labour force
+# "Labo.M" = Proportion of males in the labour force
+colnames(gii) <- c('GII.R','Country','GII','Mat.Mor','Ado.Birth','Parli.F',
+                  'Edu2.F','Edu2.M','Labo.F','Labo.M')
+
+# 5 create two new variables
+# "Edu2.FM" = Edu2.F / Edu2.M
+# "Labo.FM" = Labo2.F / Labo2.M
+Edu2.FM <- gii$Edu2.F/gii$Edu2.M   #the ratio of Female and Male populations with secondary education in each country
+Labo.FM <- gii$Labo.F/gii$Labo.M #the ratio of labour force participation of females and males in each country 
+# 5 Mutate the “Gender inequality” data
+gii_add <- mutate(gii, rpfm = Edu2.FM, rlfm = Labo.FM)
+str(gii_add)
+
+# 6 Join together the two datasets 
+# using the variable Country as the identifier
+human <- inner_join(hd, gii_add, by = "Country", suffix = c('.hd','.gii'))
+
+# 6 The joined data should have 195 observations and 19 variables
+dim(human) #195*19
+
+# 6 Call the new joined data "human" 
+# 6 and save it in your data folder
+write.csv(human, 'data/human.csv')
+
+# Data wrangling (max 5 points)
+# 1 Load the last week produced 'human.csv'
+human <- read.csv('data/human.csv')
+head(human,n=6)
+
+# 1 Explore the structure and the dimensions of the data 
+str(human);
+dim(human) #195*19
+
+# 1 and describe the dataset briefly
+# the human dataset contains 195 observations and 19 variables
+summary(human)
+# Here is the lable for the column names
+# "Country" = Country name
+# Health and knowledge
+# "GNI" = Gross National Income per capita
+# 'MYE' = "Mean.Years.of.Education"
+# "Life.Exp" = Life expectancy at birth
+# "Edu.Exp" = Expected years of schooling 
+# "Mat.Mor" = Maternal mortality ratio
+# "Ado.Birth" = Adolescent birth rate
+# "Parli.F" = Percetange of female representatives in parliament
+# "Edu2.F" = Proportion of females with at least secondary education
+# "Edu2.M" = Proportion of males with at least secondary education
+# "Labo.F" = Proportion of females in the labour force
+# "Labo.M" = Proportion of males in the labour force
+
+# 1 transform the Gross National Income (GNI) variable to numeric
+library(stringr)
+human$GNI <- strtoi(human$GNI, base=0L)
+
+# 2 Exclude unneeded variables
+# "Country", "Edu2.FM", "Labo.FM", "Edu.Exp", "Life.Exp", 
+# "GNI", "Mat.Mor", "Ado.Birth", "Parli.F"
+
+
+# 3. Remove all rows with missing values
+
+# 4. Remove the observations which relate to regions instead of countries
+
+# 5. Define the row names by the country names 
+
+# 5. remove the country name column from the data.
